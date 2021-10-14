6ceb093b668f9db99e8895ee30959d1e8e17830c
rblinde <robertblinde@gmail.com>
2021-10-14 21:24:04 +0200

    :sparkles: Added basic drawing functionality

--- src/Sketch.ts
+++ src/Sketch.ts
@@ -1,10 +1,18 @@
+interface MouseObject {
+  x: number;
+  y: number;
+}
+
 class Sketch {
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
+  isDrawing: boolean;
+  mouse: MouseObject;
 
   constructor(container: string) {
     this.canvas = document.getElementById(container) as HTMLCanvasElement;
     this.ctx = this.canvas.getContext('2d');
+    this.isDrawing = false;
   }
 
   init(): void {
@@ -14,11 +22,40 @@ class Sketch {
 
   addEventListeners(): void {
     window.addEventListener('resize', () => this.handleResize());
+    this.canvas.addEventListener('mousedown', (e) => this.handleMousedown(e));
+    this.canvas.addEventListener('mousemove', (e) => this.draw(e));
+    this.canvas.addEventListener('mouseup', () => this.isDrawing = false);
+    this.canvas.addEventListener('mouseleave', () => this.isDrawing = false);
   }
 
   handleResize(): void {
+    // Save and load current drawing
+    const tempData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
+    this.ctx.putImageData(tempData, 0, 0);
+    // Reset default settings, ctx resets after resize
+    this.ctx.lineJoin = 'round';
+    this.ctx.lineCap = 'round';
+    this.ctx.lineWidth = 10;
+  }
+
+  handleMousedown(e: MouseEvent): void {
+    this.isDrawing = true;
+    this.mouse = { x: e.offsetX, y: e.offsetY };
+    this.draw(e);
+  }
+
+  draw(e: MouseEvent): any {
+    if (!this.isDrawing) {
+      return false;
+    }
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.mouse.x, this.mouse.y);
+    this.ctx.lineTo(e.offsetX, e.offsetY);
+    this.ctx.stroke();
+    this.mouse = { x: e.offsetX, y: e.offsetY };
   }
 }
 
